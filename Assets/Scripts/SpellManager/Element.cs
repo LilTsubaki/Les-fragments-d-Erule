4272a27c5786b96ffee3238ab7ec599fc952bd50
Drazatlam <lulu.brisard@hotmail.fr>
2016-01-28 12:31:58 +0100

    Constructor ElementNode

--- Assets/Scripts/SpellManager/Element.cs
+++ Assets/Scripts/SpellManager/Element.cs
@@ -1,11 +1,38 @@
 ﻿using System;
-public enum Element{
+using System.Collections.Generic;
+using System.Collections;
+public class Element{
 
-	Fire=0,
-	Water=1,
-	Air=2,
-	Earth=3,
-	Wood=4,
-	Metal=5
+	public readonly int _id;
+	public readonly string _name;
+
+	private static List<Element> Elements;
+
+	private Element(string name){
+		_id = Elements.Count;
+		_name = name;
+	}
+
+	public static Element GetElement(int id){
+		if (Elements == null)
+			Init();
+		return  Elements[id];
+	}
+
+	public static List<Element> GetElements(){
+		if (Elements == null)
+			Init();
+		return Elements;
+	}
+
+	public static void Init(){
+		Elements = new List<Element> ();
+		Elements.Add(new Element ("Fire"));
+		Elements.Add(new Element ("Water"));
+		Elements.Add(new Element ("Air"));
+		Elements.Add(new Element ("Earth"));
+		Elements.Add(new Element ("Wood"));
+		Elements.Add(new Element ("Metal"));
+}
 		
 }
--- Assets/Scripts/SpellManager/ElementNode.cs
+++ Assets/Scripts/SpellManager/ElementNode.cs
@@ -6,25 +6,32 @@ public class ElementNode{
 
 	private Dictionary<Element,ElementNode> _nodes;
 
+	public ElementNode():this(Element.GetElement (0)){}
+
 	public ElementNode(Element element){
-		//TODO
+		_nodes = new Dictionary<Element, ElementNode> ();
+		foreach (var elem in Element.GetElements()) {
+			if (element._id <= elem._id) {
+				_nodes [elem] = null;
+			}
+		}
 	}
 
-	public SelfSpell getSelfSpell(List<Element> elements){
+	public SelfSpell GetSelfSpell(List<Element> elements){
 		//TODO
 		return null;
 	}
 
-	public TargetSpell getTargetSpell(List<Element> elements){
+	public TargetSpell GetTargetSpell(List<Element> elements){
 		//TODO
 		return null;
 	}
 
-	public void setSelfSpell(SelfSpell selfSpell, List<Element> elements){
+	public void SetSelfSpell(SelfSpell selfSpell, List<Element> elements){
 		//TODO
 	}
 
-	public void setTargetSpell(TargetSpell targetSpell, List<Element> elements){
+	public void SetTargetSpell(TargetSpell targetSpell, List<Element> elements){
 		//TODO
 	}
 		
