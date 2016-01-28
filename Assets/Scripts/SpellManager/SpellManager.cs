36c82801645bc7371ba0b753f40fabd21cc15e58
LilTsubaki <omenforce.clem@neuf.fr>
2016-01-28 11:40:39 +0100

    spellManager

---
+++ Assets/Scripts/SpellManager/SpellManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class SpellManager
+{
+    private SpellManager _spellManager;
+
+    private Dictionary<int, Area> _areas;
+    private Dictionary<int, Range> _ranges;
+    private Dictionary<int, Effect> _effects;
+    private ElementNode _elementNode;
+
+    private SpellManager() { }
+
+    public SpellManager getInstance()
+    {
+        if (_spellManager == null)
+            _spellManager = new SpellManager();
+
+        return _spellManager;
+    }
+
+}
