65097931395b7d88c8d6bba51709039d5a6f4d9c
Drazatlam <lulu.brisard@hotmail.fr>
2016-01-28 11:40:41 +0100

    ElementNode

---
+++ Assets/Scripts/SpellManager/ElementNode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ElementNode{
+
+	private Dictionary<Element,ElementNode> _nodes;
+
+	public ElementNode(Element element){
+		//TODO
+	}
+
+	public SelfSpell getSelfSpell(List<Element> elements){
+		//TODO
+	}
+
+	public TargetSpell getTargetSpell(List<Element> elements){
+		//TODO
+	}
+
+	public void setSelfSpell(SelfSpell selfSpell, List<Element> elements){
+		//TODO
+	}
+
+	public void setTargetSpell(TargetSpell targetSpell, List<Element> elements){
+		//TODO
+	}
+		
+}
---
+++ Assets/Scripts/SpellManager/EmptyEnumeration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public enum Element{
+
+	Fire=0,
+	Water=1,
+	Air=2,
+	Earth=3,
+	Wood=4,
+	Metal=5
+		
+}
