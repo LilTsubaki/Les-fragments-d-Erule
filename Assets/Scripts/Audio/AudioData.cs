1561f222a8222718babd29acdd1b341640c9abfc
Bosphoros <floriantd.adam@gmail.com>
2016-01-29 13:40:06 +0100

    AudioManager with self pooling system

---
+++ Assets/Scripts/Audio/AudioData.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioData : MonoBehaviour
+{
+
+    public string channel;
+    public List<AudioClip> clips;
+    public bool random;
+    private int seq = 0;
+    public int priority;
+    public float pitchMin;
+    public float pitchMax;
+    public AudioMixerGroup group;
+
+    void Awake()
+    {
+        AudioManager.GetInstance().Register(channel, this);
+    }
+
+    void OnDestroy()
+    {
+        AudioManager.GetInstance().Withdraw(channel);
+    }
+
+    public AudioClip GetClip()
+    {
+        AudioClip clip;
+        if (random)
+        {
+            int id = Random.Range(0, clips.Count - 1);
+            clip = clips[id];
+        }
+        clip = clips[seq];
+
+        seq = (seq + 1) % clips.Count;
+        return clip;
+    }
+
+}
---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AudioPlayer : MonoBehaviour {
+
+    public AudioSource _audio;
+    public float _power;
+    public bool _fadeIn;
+    public bool _fadeOut;
+    
+	// Use this for initialization
+	void Start () {
+        _power = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_fadeIn)
+        {
+            _power = Mathf.Min(1, _power + Time.deltaTime);
+            if(_power == 1)
+            {
+                _fadeIn = false;
+            }
+        }
+        else if (_fadeOut)
+        {
+            _power = Mathf.Max(0, _power - Time.deltaTime);
+            if (_power == 0)
+            {
+                _fadeOut = false;
+            }
+        }
+
+        _audio.volume = _power;
+    }
+
+    public void FadeIn()
+    {
+        _fadeOut = false;
+        _fadeIn = true;
+        _power = 0;
+    }
+
+    public void FadeOut()
+    {
+        _fadeOut = true;
+        _fadeIn = false;
+    }
+}
