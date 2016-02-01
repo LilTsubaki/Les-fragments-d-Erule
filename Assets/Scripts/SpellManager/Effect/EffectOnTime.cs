0c6223bed9e7641180da501f373d9b3041c4764b
Drazatlam <lulu.brisard@hotmail.fr>
2016-02-01 15:28:29 +0100

    EffectOnTime

---
+++ Assets/Scripts/SpellManager/Effect/EffectOnTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class EffectOnTime : Effect
+{
+	private EffectDirect _effectDirect;
+	private uint _nbTurn;
+
+	public EffectOnTime (EffectDirect effectDirect, uint nbTurn)
+	{
+		_effectDirect = effectDirect;
+		_nbTurn = nbTurn;
+	}
+}
+
