f7208e3717146ad49193213539993c13a3ebecfa
Drazatlam <lulu.brisard@hotmail.fr>
2016-02-02 09:41:27 +0100

    protection dictionary in Character

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Character
 {
@@ -6,8 +7,18 @@ public class Character
 	public uint _lifeCurrent;
 	Hexagon _position;
 	public uint _currentActionPoints;
+	Dictionary<Element, uint> _protections;
+	Dictionary<Element, uint> _protectionsNegative;
 
-	public Character ()
+	public Character (uint lifeMax, Hexagon position)
 	{
+		_lifeMax = lifeMax;
+		_lifeCurrent = lifeMax;
+		_position = position;
+
+		foreach (var e in Element.GetElements()) {
+			_protections [e] = 0;
+			_protectionsNegative [e] = 0;
+		}
 	}
 }
