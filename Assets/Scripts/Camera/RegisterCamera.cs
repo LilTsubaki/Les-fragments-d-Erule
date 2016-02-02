8b164ee5ee09258a15e993463dd3c5a5db1a5396
Bosphoros <floriantd.adam@gmail.com>
2016-02-02 11:01:40 +0100

    End documentation of CameraManager and associated scripts

--- Assets/Scripts/Camera/RegisterCamera.cs
+++ Assets/Scripts/Camera/RegisterCamera.cs
@@ -1,22 +1,51 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class RegisterCamera : MonoBehaviour
 {
 
+    /// <summary>
+    /// The name to register the Camera under.
+    /// </summary>
     public string _cameraName;
+    /// <summary>
+    /// The registered Camera.
+    /// </summary>
     Camera _camera;
 
     // Zoom values
+    /// <summary>
+    /// Is there a transition being played on the orthographic size ?
+    /// </summary>
     bool _isChangingOrthoSize;
+    /// <summary>
+    /// The time the transition started.
+    /// </summary>
     float _startingChangeTime;
+    /// <summary>
+    /// The orthographic size the Camera is changing to.
+    /// </summary>
     float _orthographicSize;
+    /// <summary>
+    /// The starting orthographic size, before a transition.
+    /// </summary>
     float _orthographicSizePrevious;
+    /// <summary>
+    /// The time of the zooming transition.
+    /// </summary>
     float _speedZoom;
 
     // Movement values
+    /// <summary>
+    /// Is there a transition being played on the Camera position ?
+    /// </summary>
     bool _isMoving;
+    /// <summary>
+    /// The destination of the Camera.
+    /// </summary>
     Vector3 _posAimed;
+    /// <summary>
+    /// The time of the moving transition.
+    /// </summary>
     float _speedMove;
 
     void Start()
@@ -37,20 +66,31 @@ public class RegisterCamera : MonoBehaviour
         CheckZoom();
     }
 
+    /// <summary>
+    /// Checks if the Camera has to be moved and changes its position if needed.
+    /// </summary>
     void CheckMove()
     {
         if(_isMoving){
-            Vector3 p = Vector3.MoveTowards(gameObject.transform.position, _posAimed, _speedMove);
+            Vector3 p = Vector3.MoveTowards(gameObject.transform.position, _posAimed, Time.deltaTime * _speedMove);
             gameObject.transform.position = p;
             _isMoving = !p.Equals(_posAimed);
         }
     }
 
+    /// <summary>
+    /// Checks if the Camera has to be zoomed and changes its orthographic size if needed.
+    /// </summary>
     void CheckZoom()
     {
         if (_isChangingOrthoSize)
         {
-            _camera.orthographicSize = Mathf.Lerp(_orthographicSizePrevious, _orthographicSize, (Time.time - _startingChangeTime) * _speedZoom);
+            float newSize = Mathf.Lerp(_orthographicSizePrevious, _orthographicSize, (Time.time - _startingChangeTime) * _speedZoom);
+            if(_camera.orthographicSize >= 0 && newSize < 0)
+            {
+                Logger.Warning("The Camera " + _cameraName + " is now reversed.");
+            }
+            _camera.orthographicSize = newSize;
             if (_camera.orthographicSize.Equals(_orthographicSize))
             {
                 _orthographicSizePrevious = _orthographicSize;
@@ -59,20 +99,29 @@ public class RegisterCamera : MonoBehaviour
         }
     }
 
-
+    /// <summary>
+    /// Changes the state of the Camera in order to make it move.
+    /// </summary>
+    /// <param name="pos">The destination of the Camera.</param>
+    /// <param name="speed">The duration of the movement, in seconds.</param>
     public void MoveTo(Vector3 pos, float speed)
     {
         _isMoving = true;
         _posAimed = pos;
-        _speedMove = speed;
+        _speedMove = 1/speed;
     }
 
+    /// <summary>
+    /// Changes the state of the Camera in order to make it zoom. Only with orthographic Cameras.
+    /// </summary>
+    /// <param name="orthoSize">The wanted final orthographic size.</param>
+    /// <param name="speed">The duration of the zoom, in seconds.</param>
     public void ZoomOrthoTo(float orthoSize, float speed)
     {
         _isChangingOrthoSize = true;
         _startingChangeTime = Time.time;
         _orthographicSize = orthoSize;
         _orthographicSizePrevious = _camera.orthographicSize;
-        _speedZoom = speed;
+        _speedZoom = 1/speed;
     }
 }
--- Assets/Scripts/Camera/RegisterFadeScreen.cs
+++ Assets/Scripts/Camera/RegisterFadeScreen.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 
 public class RegisterFadeScreen : MonoBehaviour
 {
-
+    /// <summary>
+    /// The image of the panel. Its alpha value will be modified.
+    /// </summary>
     Image _image;
+    /// <summary>
+    /// Is it fading in ?
+    /// </summary>
     bool _fadeIn;
+    /// <summary>
+    /// Remaining time of the transition.
+    /// </summary>
     float _remainingTime;
+    /// <summary>
+    /// The total time of the transition.
+    /// </summary>
+    float _speed;
+
+    /// <summary>
+    /// The name of the trigger to activate after the transition.
+    /// </summary>
+    string _anim;
 
     void Start()
     {
@@ -29,36 +45,79 @@ public class RegisterFadeScreen : MonoBehaviour
         }
     }
 
-    public void Reverse()
+    public void SetAnimation(string anim)
+    {
+        _anim = anim;
+    }
+
+    /// <summary>
+    /// Resets the transition values and reverses the state of fading in/out.
+    /// </summary>
+    /// <param name="speed"></param>
+    public void Reverse(float speed)
     {
         _fadeIn = !_fadeIn;
         _remainingTime = 1;
+        _speed = 1/(speed*0.5f);
     }
 
+    /// <summary>
+    /// Diminishes the alpha channel of the image according to the transition speed.
+    /// </summary>
     void FadeIn()
     {
-        float a = Mathf.Clamp01(_remainingTime / 1);
+        float a = Mathf.Clamp01(_remainingTime);
         Color c = new Color(_image.color.r, _image.color.g, _image.color.b, a);
         _image.color = c;
-        _remainingTime = Mathf.Clamp01(_remainingTime - Time.deltaTime);
+        _remainingTime = Mathf.Clamp01(_remainingTime - Time.deltaTime*_speed);
     }
 
+    /// <summary>
+    /// Increases the alpha channel of the image according to the transition speed.
+    /// </summary>
     void FadeOut()
     {
         float a = Mathf.Clamp01(1 - _remainingTime / 1);
         Color c = new Color(_image.color.r, _image.color.g, _image.color.b, a);
         _image.color = c;
-        _remainingTime = Mathf.Clamp01(_remainingTime - Time.deltaTime);
+        _remainingTime = Mathf.Clamp01(_remainingTime - Time.deltaTime*_speed);
     }
 
-    public void FadeTime()
+    /// <summary>
+    /// Invokes the end of the transition after the specified time.
+    /// </summary>
+    /// <param name="transitionTime">The time to end the transition after.</param>
+    public void FadeTime(float transitionTime)
     {
-        Invoke("FadeEnd", 1);
+        Invoke("FadeEnd", transitionTime);
     }
 
+    /// <summary>
+    /// Invokes the end of the transition after the specified time when having a trigger to activate.
+    /// </summary>
+    /// <param name="speed"></param>
+    public void FadeTimeAnim(float speed)
+    {
+        Invoke("FadeEndStartAnim", speed);
+    }
+
+
+    /// <summary>
+    /// Puts an end to the transition and activates the trigger previously specified.
+    /// </summary>
+    public void FadeEndStartAnim()
+    {
+        Reverse(_speed);
+        CameraManager.GetInstance().ActivateMainStartAnim(_anim);
+        _anim = "";
+    }
+
+    /// <summary>
+    /// Puts an end to the transition.
+    /// </summary>
     void FadeEnd()
     {
-        Reverse();
+        Reverse(_speed);
         CameraManager.GetInstance().ActivateMain();
     }
 
