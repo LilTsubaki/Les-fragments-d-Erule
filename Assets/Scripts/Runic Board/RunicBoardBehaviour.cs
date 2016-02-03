d82ecf02d4054ce36c063ac13f7891108c29d84b
Mythique <sylvain.venereux@gmail.com>
2016-02-03 15:27:17 +0100

    Placing runes in hand into Unity

--- Assets/Scripts/Runic Board/RunicBoardBehaviour.cs
+++ Assets/Scripts/Runic Board/RunicBoardBehaviour.cs
@@ -5,12 +5,53 @@ using System.Collections.Generic;
 public class RunicBoardBehaviour : MonoBehaviour {
     private RunicBoard _board;
 
-    public GameObject FireRuneAsset;
-    public GameObject WaterRuneAsset;
-    public GameObject AirRuneAsset;
-    public GameObject EarthRuneAsset;
-    public GameObject WoodRuneAsset;
-    public GameObject MetalRuneAsset;
+    public GameObject _handGO;
+    public GameObject _boardGO;
+
+    public GameObject _airRuneAsset;
+    public GameObject _earthRuneAsset;
+    public GameObject _fireRuneAsset;
+    public GameObject _metalRuneAsset;
+    public GameObject _woodRuneAsset;
+    public GameObject _waterRuneAsset;
+
+    private void DisplayRunesInHand()
+    {
+        for (int i = 0; i < _board.RunesInHand.Count; i++)
+        {
+            Transform parent = _handGO.transform.GetChild(i);
+
+            GameObject rune = new GameObject();
+            
+            switch(_board.RunesInHand[i]._element._name)
+            {
+                case "Fire":
+                    rune = GameObject.Instantiate(_fireRuneAsset);
+                    break;
+                case "Water":
+                    rune = GameObject.Instantiate(_waterRuneAsset);
+                    break;
+                case "Air":
+                    rune = GameObject.Instantiate(_airRuneAsset);
+                    break;
+                case "Earth":
+                    rune = GameObject.Instantiate(_earthRuneAsset);
+                    break;
+                case "Wood":
+                    rune = GameObject.Instantiate(_woodRuneAsset);
+                    break;
+                case "Metal":
+                    rune = GameObject.Instantiate(_metalRuneAsset);
+                    break;
+                default:
+                    break;
+            }
+
+            rune.transform.SetParent(parent);
+            rune.transform.localPosition = new Vector3(0, 0.3f, 0);
+            rune.transform.Rotate(new Vector3(0, 1, 0), 90);
+        }
+    }
 
     void Awake()
     {
@@ -25,15 +66,19 @@ public class RunicBoardBehaviour : MonoBehaviour {
         hand.Add(r4);
         Rune r5 = new Rune(Element.GetElement(3), -1);
         hand.Add(r5);
-        Rune r6 = new Rune(Element.GetElement(3), -1);
+        Rune r6 = new Rune(Element.GetElement(4), -1);
         hand.Add(r6);
+        Rune r7 = new Rune(Element.GetElement(3), -1);
+        hand.Add(r7);
 
         _board = new RunicBoard(hand);
+
+        DisplayRunesInHand();
     }
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
