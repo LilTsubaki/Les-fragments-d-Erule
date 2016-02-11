99015fd3598dceb22a9f25bea411be7adca01b80
Bosphoros <floriantd.adam@gmail.com>
2016-02-11 10:09:23 +0100

    Added Turn UI

---
+++ Assets/Scripts/UI/UITurn.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UITurn : MonoBehaviour {
+
+    public Text _turnText;
+    public Text _playerName;
+	
+	// Update is called once per frame
+	void Update () {
+        Character c = PlayBoardManager.GetInstance().GetCurrentPlayer();
+        _turnText.text = c.TurnNumber.ToString();
+        _playerName.text = c.Name;
+	}
+}
