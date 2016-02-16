8859f34c9f0c01d5f2e26ee30157189afd90ef0b
FlorioA <adrien.florio@gmail.com>
2016-02-16 15:04:19 +0100

    fixe bug

--- Assets/Scripts/PlayBoard/TurnManager.cs
+++ Assets/Scripts/PlayBoard/TurnManager.cs
@@ -76,7 +76,7 @@ public class TurnManager
 
         _turnNumber++;
         //PlayBoardManager.GetInstance().Board.ResetBoard();
-
+        PlayBoardManager.GetInstance().Board.ResetBoard();
         BeginTurn();
         //penser à appliquer les effets des buffs/debuffs sur les joeuurs
         //+ reset ce qu'il y a à reset
