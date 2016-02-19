d4608f1de0ad0087a5b9711fedf71f2c930a9207
Bosphoros <floriantd.adam@gmail.com>
2016-02-19 09:51:25 +0100

    Set names for players

--- Assets/Scripts/TestScripts/Sylvain/TestSpawnNetwork.cs
+++ Assets/Scripts/TestScripts/Sylvain/TestSpawnNetwork.cs
@@ -23,6 +23,8 @@ public class TestSpawnNetwork : MonoBehaviour {
         _playBoard = JSONObject.JSONToBoard(ref _board, _boardName);
         _player1 = new Character(4000, _player1GameObject);
         _player2 = new Character(4000, _player2GameObject);
+        _player1.Name = "Player 1";
+        _player2.Name = "Player 2";
 
         _gameStarted = false;
 
