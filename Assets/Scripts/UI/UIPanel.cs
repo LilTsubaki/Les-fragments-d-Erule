2a104ff99f1073af0250d067f06036e46afb75c2
Bosphoros <floriantd.adam@gmail.com>
2016-02-25 11:42:04 +0100

    Updated UIPanel to use CanvasGroup

--- Assets/Scripts/UI/UIPanel.cs
+++ Assets/Scripts/UI/UIPanel.cs
@@ -10,29 +10,69 @@ public class UIPanel : MonoBehaviour
     public string panelID;
     public bool hideOnLoad = true;
     public float fadeSpeed;
-    
+
+    private float _now;
+    private bool _isFadingIn;
+    private bool _isFadingOut;
+    private CanvasGroup _group;
+
     void Start()
     {
         UIManager.GetInstance().RegisterPanel(gameObject, panelID);
-        UIManager.GetInstance().ShowPanel(panelID);
+        UIManager.GetInstance().ShowPanelNoStack(panelID);
         if (hideOnLoad)
         {
-            UIManager.GetInstance().HidePanel(panelID);
+            UIManager.GetInstance().HidePanelNoStack(panelID);
+        }
+        _isFadingIn = false;
+        _isFadingOut = false;
+
+        _group = gameObject.AddComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+        if (_isFadingIn)
+        {
+            _now += Time.deltaTime;
+            float alpha = _now / fadeSpeed;
+            _group.alpha = alpha;
+            if(alpha > 1)
+            {
+                _isFadingIn = false;
+            }
+        }
+        if(_isFadingOut)
+        {
+            _now += Time.deltaTime;
+            float alpha = 1 - (_now / fadeSpeed);
+            _group.alpha = alpha;
+            if (alpha < 0)
+            {
+                _isFadingOut = false;
+                gameObject.SetActive(false);
+            }
         }
     }
 
     public void FadeIn()
     {
-        Image im = gameObject.GetComponent<Image>();
+        _isFadingOut = false;
+        _isFadingIn = true;
+        _now = 0;
+        /*Image im = gameObject.GetComponent<Image>();
         im.canvasRenderer.SetAlpha(0);
-        im.CrossFadeAlpha(1, fadeSpeed , true);
+        im.CrossFadeAlpha(1, fadeSpeed , true);*/
     }
 
     public void FadeOut()
     {
-        Image im = gameObject.GetComponent<Image>();
+        _isFadingIn = false;
+        _isFadingOut = true;
+        _now = 0;
+        /*Image im = gameObject.GetComponent<Image>();
         float alpha = im.canvasRenderer.GetAlpha();
-        im.CrossFadeAlpha(0, fadeSpeed * alpha, true);
+        im.CrossFadeAlpha(0, fadeSpeed * alpha, true);*/
     }
 
     void Awake()
