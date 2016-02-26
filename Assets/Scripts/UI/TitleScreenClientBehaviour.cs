8ff8d3b432892514d9d1f625cff643ee4896c856
Mythique <sylvain.venereux@gmail.com>
2016-02-26 09:30:13 +0100

    Update final scene for server, and moved old scenes in a specific folder

---
+++ Assets/Scripts/UI/TitleScreenClientBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenClientBehaviour : MonoBehaviour {
+
+    public GameObject _deckSelectionGo;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UIManager.GetInstance().HidePanelNoStack("PanelTitleScreen");
+            UIManager.GetInstance().ShowPanelNoStack("PanelRunicBoard");
+            _deckSelectionGo.SetActive(true);
+        }
+    }
+}
