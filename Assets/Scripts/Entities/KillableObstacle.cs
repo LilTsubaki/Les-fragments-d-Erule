8eae5f57c557f6b4f82a70222f6b0423762f75f9
LilTsubaki <omenforce.clem@neuf.fr>
2016-02-26 09:57:58 +0100

    killableobstacle

---
+++ Assets/Scripts/Entities/KillableObstacle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class KillableObstacle : Obstacle, Killable
+{
+    private int _maxLife;
+    private int _currentLife;
+
+    
+
+    public KillableObstacle(Hexagon position, int life) : base(position)
+    {
+        MaxLife = life;
+        CurrentLife = life;
+    }
+
+
+    public int ReceiveDamage(int value, Element element)
+    {
+        if (_currentLife - value < 0)
+            _currentLife = 0;
+        else
+            _currentLife -= value;
+
+        Logger.Debug("Receive damage value : " + value + " for element : " + element._name);
+        return value;
+    }
+    public bool isDead()
+    {
+        return CurrentLife <= 0;
+    }
+
+    public int MaxLife
+    {
+        get
+        {
+            return _maxLife;
+        }
+
+        set
+        {
+            _maxLife = value;
+        }
+    }
+
+    public int CurrentLife
+    {
+        get
+        {
+            return _currentLife;
+        }
+
+        set
+        {
+            _currentLife = value;
+        }
+    }
+}
+
