8f8d5c77ec0ac9f133d11ba81aa7fa9b898844a0
Mythique <sylvain.venereux@gmail.com>
2016-03-24 16:40:11 +0100

    Portals updated Now displays a blank texture if only one portal exists Portals now teleport correctly

--- Assets/Scripts/PlayBoard/PortalManager.cs
+++ Assets/Scripts/PlayBoard/PortalManager.cs
@@ -13,15 +13,19 @@ public class PortalManager {
         if (_instance == null)
         {
             _instance = new PortalManager();
+            _instance.Init();
         }
 
         return _instance;
     }
 
-    public void Init()
+    private void Init()
     {
-        GameObject portal1GO = Resources.Load<GameObject>("prefabs/Portal_1");
-        GameObject portal2GO = Resources.Load<GameObject>("prefabs/Portal_2");
+        GameObject portal1Prefab = Resources.Load<GameObject>("prefabs/Portal_1");
+        GameObject portal1GO = GameObject.Instantiate(portal1Prefab);
+        GameObject portal2Prefab = Resources.Load<GameObject>("prefabs/Portal_2");
+        GameObject portal2GO = GameObject.Instantiate(portal2Prefab);
+
         _portal1 = new Portal(null, portal1GO, 1);
         _portal1.GameObject.SetActive(false);
         _portal2 = new Portal(null, portal2GO, 2);
@@ -49,6 +53,31 @@ public class PortalManager {
                 _portal1.ActivatePortal(position);
             }
         }
+
+        if (TwoPortalsActivated())
+        {
+            if (_portal1.GameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture == null)
+            {
+                _portal1.GameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture = Resources.Load<RenderTexture>("images/TextureFromPortal2");
+            }
+
+            if (_portal2.GameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture == null)
+            {
+                _portal2.GameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture = Resources.Load<RenderTexture>("images/TextureFromPortal1");
+            }
+        }
+        else
+        {
+            if(_portal1.IsActive())
+            {
+                _portal1.GameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture = null;
+            }
+
+            if (_portal2.IsActive())
+            {
+                _portal2.GameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture = null;
+            }
+        }
     }
 
     public bool TwoPortalsActivated()
@@ -58,21 +87,27 @@ public class PortalManager {
 
     public void Teleport(Character character)
     {
+        Logger.Debug("2 portals ? " +TwoPortalsActivated());
+        Logger.Debug("Char on portal ? " + (character.Position.Portal != null));
         if (TwoPortalsActivated() && character.Position.Portal != null)
         {
             if (character.Position.Portal == _portal1)
             {
-                if (_portal2.Position._entity != null)
+                if (_portal2.Position._entity == null)
                 {
+                    Logger.Debug("On portal 1, portal 2 has no entity");
                     character.Position = _portal2.Position;
+                    _portal1.GameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture = null;
                     _portal2.Destroy();
                 }
             }
             else
             {
-                if(_portal1.Position._entity != null)
+                if(_portal1.Position._entity == null)
                 {
+                    Logger.Debug("On portal 2, portal 1 has no entity");
                     character.Position = _portal1.Position;
+                    _portal2.GameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material.mainTexture = null;
                     _portal1.Destroy();
                 }
             }
