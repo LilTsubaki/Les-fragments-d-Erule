828242ea9366eacc673ad49a6cc21443b8bb0bae
Nyumesis <thenyomesis@gmail.com>
2016-03-30 17:13:53 +0200

    VFX Water / Cast

---
+++ Assets/resources/Shaders/movingAlphaMask_main water.shader
@@ -0,0 +1,42 @@
+﻿Shader "Custom/Transparent Diffuse (Mask cutoff)_main water" {
+	Properties{
+		_MainTex("Base (RGB) Trans (A)", 2D) = "white" {}
+		_Cutoff("Mask cutoff", Range(1,0)) = 0
+		_MaskTex("Mask", 2D) = "white" {}
+	}
+
+	SubShader{
+		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent" }
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		CGPROGRAM
+		#pragma surface surf Lambert alpha
+
+		sampler2D _MainTex;
+		sampler2D _MaskTex;
+		half _Cutoff;
+
+		struct Input {
+			float2 uv_MainTex;
+			float2 uv_MaskTex;
+		};
+
+		void surf(Input IN, inout SurfaceOutput o) {
+
+			// Alpha cutoff
+
+			half mask = tex2D(_MaskTex, IN.uv_MaskTex);
+			fixed4 c = tex2D(_MainTex, IN.uv_MainTex);
+
+			clip(mask - _Cutoff * 1.004);
+
+			o.Albedo = c.rgb*3;
+			o.Alpha = c.a;
+		}
+
+		ENDCG
+	}
+
+	Fallback "Transparent/Diffuse"
+}
--- Assets/resources/VFX/Wood/Script/VFX_Wood_ThorncrownRotate.cs
+++ Assets/resources/VFX/Wood/Script/VFX_Wood_ThorncrownRotate.cs
@@ -4,24 +4,25 @@ using System.Collections;
 
 public class VFX_Wood_ThorncrownRotate : MonoBehaviour {
 
-		//public GameObject Thorncrown;
+	//public GameObject Thorncrown;
 			
-		private float Yrot,rot1,tilt1 = 0.0f;
-        [Range(-30.0f,30.0f)]public float rotSpeed1 = 3.0f;
-		[Range(-30.0f,30.0f)]public float tiltSpeed1 = 10.0f;
+	private float Yrot,rot1,tilt1 = 0.0f;
+	[Range(-30.0f,30.0f)]public float rotSpeed1 = 3.0f;
+	[Range(-30.0f,30.0f)]public float tiltSpeed1 = 10.0f;
 
 
     void Start()
-        {
-            Yrot = /*Thorncrown*/gameObject.transform.eulerAngles.y;
-            Debug.Log(Yrot);
-        }
+    {
+        Yrot = /*Thorncrown*/gameObject.transform.localEulerAngles.y;
+        Debug.Log(Yrot);
+    }
+
     void FixedUpdate()
-		{	
-			rot1 += rotSpeed1;
-			tilt1 = Mathf.Sin (Time.time) * tiltSpeed1;
-				
-			/*Thorncrown*/gameObject.transform.eulerAngles = new Vector3 (rot1,Yrot,tilt1);	// replace the Y coordinates by rot
-		}
+	{	
+		rot1 += rotSpeed1;
+		tilt1 = Mathf.Sin (Time.time) * tiltSpeed1;
+			
+		/*Thorncrown*/gameObject.transform.localEulerAngles = new Vector3 (rot1,Yrot,tilt1);	// replace the Y coordinates by rot
+	}
 
 }
