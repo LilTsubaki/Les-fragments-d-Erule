14b42d77bc04dacab5e3996966e22e836a691b2e
Bosphoros <floriantd.adam@gmail.com>
2016-04-03 23:06:37 +0200

    Added script for orb animation

---
+++ Assets/Scripts/Animation/RotatingOrb.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingOrb : MonoBehaviour {
+
+    public float _rotationSpeed;
+
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.RotateAround(gameObject.transform.parent.position, gameObject.transform.right, _rotationSpeed);
+	}
+}
