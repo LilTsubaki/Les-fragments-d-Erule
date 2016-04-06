32a08e7574219bc576ce9b1ecb671bb550e32595
Bosphoros <floriantd.adam@gmail.com>
2016-04-06 10:00:17 +0200

    Added animations for Water and added modifiable duration animation for orbs

--- Assets/Scripts/SpellAnimations/WaterAnimation.cs
+++ Assets/Scripts/SpellAnimations/WaterAnimation.cs
@@ -4,10 +4,14 @@ using System.Collections;
 
 public class WaterAnimation : SpellAnimation 
 {
-	
+
+    public Animator _waterAnimator;
+
 	public ParticleSystem _splashesBottom;
 	public ParticleSystem _splashesTop;
 	public ParticleSystem _water;
+    public ParticleSystem _foamBottom;
+    public ParticleSystem _foam;
     public Renderer _waterMat;
 
     [Range(0, 1)]
@@ -16,12 +20,14 @@ public class WaterAnimation : SpellAnimation
 	// Update is called once per frame
 	void Update () {
 		if (_play) {
+
 			gameObject.transform.position = _to;
 			Vector3 projCam = new Vector3 (Camera.main.transform.position.x, gameObject.transform.position.y, Camera.main.transform.position.z);
 			gameObject.transform.LookAt (projCam);
-            _waterMat.material.SetFloat("_Cutoff", 1 - _cutoff);
 
-            /*
+            _waterAnimator.SetTrigger("play");
+
+            
 			ParticleSystem.EmissionModule modsplashesBottom = _splashesBottom.emission;
 			modsplashesBottom.enabled = true;
 			_splashesBottom.Simulate (0, false, true);
@@ -37,17 +43,33 @@ public class WaterAnimation : SpellAnimation
 			_water.Simulate (0, false, true);
 			_water.Play ();
 
+            ParticleSystem.EmissionModule modfoamBot = _foamBottom.emission;
+            modfoamBot.enabled = true;
+            _foamBottom.Simulate(0, false, true);
+            _foamBottom.Play();
+
+            ParticleSystem.EmissionModule modfoam = _foam.emission;
+            modfoam.enabled = true;
+            _foam.Simulate(0, false, true);
+            _foam.Play();
+
+            _play = !_play;
 		}
+        _waterMat.material.SetFloat("_Cutoff", 1 - _cutoff);
 	}
-		public void ResetBursts()
-		{
+
+	public void ResetBursts() {
 		ParticleSystem.EmissionModule modsplashesBottom = _splashesBottom.emission;
 		modsplashesBottom.enabled = false;
 		ParticleSystem.EmissionModule modsplashesTop = _splashesTop.emission;
 		modsplashesTop.enabled = false;
-		ParticleSystem.EmissionModule modwater = _splashesTop.emission;
-		modwater.enabled = false;*/
-        }
-	}
+		ParticleSystem.EmissionModule modwater = _water.emission;
+		modwater.enabled = false;
+        ParticleSystem.EmissionModule modfoamBot = _foamBottom.emission;
+        modfoamBot.enabled = true;
+        ParticleSystem.EmissionModule modfoam = _foam.emission;
+        modfoam.enabled = false;
+    }
 }
+
 
