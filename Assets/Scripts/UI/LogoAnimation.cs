b45648d3ed2168b30c6757822ecbb49f4a810c33
Nyumesis <thenyomesis@gmail.com>
2016-04-12 17:01:38 +0200

    Logo with E animation

--- Assets/Scripts/UI/LogoAnimation.cs
+++ Assets/Scripts/UI/LogoAnimation.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class LogoAnimation : MonoBehaviour {
 
 	public Animator SM_E;
+	public Animator SM_E_pan;
+	public Animator SM_Bordure_E;
 	public Animator SM_Logo_Rock_2;
 	public Animator SM_Logo_Rock_3;
 	public Animator SM_Logo_Rock_4;
@@ -27,6 +29,8 @@ public class LogoAnimation : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.L)) 
 			{
 				SM_E.SetTrigger ("play");
+				SM_E_pan.SetTrigger ("play");
+				SM_Bordure_E.SetTrigger ("play");
 				SM_Logo_Rock_2.SetTrigger ("play");
 				SM_Logo_Rock_3.SetTrigger ("play");
 				SM_Logo_Rock_4.SetTrigger ("play");
