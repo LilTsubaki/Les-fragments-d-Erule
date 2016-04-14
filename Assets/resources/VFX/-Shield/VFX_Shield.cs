ff8bf28190cc4097fea17a1eb038a0b5503cb570
Mythique <sylvain.venereux@gmail.com>
2016-04-14 17:33:57 +0200

    Display shields

--- Assets/resources/VFX/-Shield/VFX_Shield.cs
+++ Assets/resources/VFX/-Shield/VFX_Shield.cs
@@ -4,11 +4,7 @@ using System.Collections;
 
 public class VFX_Shield : MonoBehaviour
 {
-
-    public Transform target;
     public GameObject character;
-
-    private Vector3 characterPos = new Vector3(0.0f,0.0f,0.0f);
 
     private float tilt4, lift4 = 0.0f;
     [Range(-0.01f, 0.01f)] public float lift4Speed = 0.004f;
@@ -16,16 +12,14 @@ public class VFX_Shield : MonoBehaviour
 
     void Start()
     {
-        characterPos = character.transform.localPosition;
-    } 
+
+    }
+
     void FixedUpdate()
     {
         lift4 = Mathf.Sin(Time.time) * lift4Speed;
 
         transform.Translate(0.0f, lift4, 0.0f, Space.Self);
-        transform.RotateAround(characterPos, Vector3.up, revolve4);
-        //Vector3 look = new Vector3(target.position.x, transform.position.y, target.position.z);
-        //transform.LookAt(target, Vector3.up);
-        //transform.Rotate(90, 0, 0);
+        transform.RotateAround(character.transform.position, Vector3.up, revolve4);
     }
 }
