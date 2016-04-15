db8bfe3ce5ff418af7825e90424e3b33ffa08c3c
Mythique <sylvain.venereux@gmail.com>
2016-04-15 09:42:33 +0200

    Random tips

---
+++ Assets/Scripts/Utils/EruleTips.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EruleTips {
+
+    private static EruleTips _instance;
+    private List<string> tips;
+
+    public static EruleTips GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new EruleTips();
+            _instance.Init();
+        }
+        return _instance;
+    }
+
+    private void Init()
+    {
+        tips = new List<string>();
+        JSONObject js = JSONObject.GetJsonObjectFromFile("JsonFiles/tips");
+        JSONObject array = js.GetField("tips");
+        foreach(JSONObject str in array.list)
+        {
+            tips.Add(str.str);
+        }
+    }
+
+    public string GetRandomTip()
+    {
+        int rand = EruleRandom.RangeValue(0, tips.Count - 1);
+        return tips[rand];
+    }
+}
